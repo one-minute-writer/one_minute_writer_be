642f0d51eda86830f9c2243152141da3c5545b77
Dylan Harper <dylanharper4@icloud.com>
2022-03-24 23:30:24 -0700

    Add validations and tests for story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,8 @@
 class Story < ApplicationRecord
-
+    validates_presence_of :user_id,
+                          :total_time,
+                          :body_text,
+                          :image,
+                          :words,
+                          :sound
 end
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 RSpec.describe Story, type: :model do
 
   it 'exists' do
-    user = create(:story)
-    expect(user).to be_a Story
+    story = create(:story)
+    expect(story).to be_a Story
   end
 
   describe 'validations' do
-
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :total_time }
+    it { should validate_presence_of :body_text }
+    it { should validate_presence_of :image }
+    it { should validate_presence_of :words }
+    it { should validate_presence_of :sound }
   end
 end

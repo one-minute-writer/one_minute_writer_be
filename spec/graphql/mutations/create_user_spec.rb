ebc1b0add34136dc8a0ff72dad1eaa589d80349b
Dylan Harper <dylanharper4@icloud.com>
2022-03-26 15:17:27 -0700

    Add test for creating a user in gql mutation

---
+++ spec/graphql/mutations/create_user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Types::QueryType do
+  describe 'create user' do
+    def query
+      <<~GQL
+            mutation {
+        addUser(input: { params: { username: "Bob", email: "bob@athens.com" } }) {
+        	user {
+          	username
+          	email
+          }
+        }
+      }
+      GQL
+    end
+
+    it 'can create a user' do
+      user_record_length = User.all.length
+      result = OneMinuteWriterBeSchema.execute(query).as_json
+      symbolized = result.deep_symbolize_keys!
+      current_user_count = User.all.length
+
+      expect(current_user_count).to eq(user_record_length + 1)
+      expect(symbolized).to be_a Hash
+      expect(symbolized[:data][:addUser][:user]).to have_key(:username)
+      expect(symbolized[:data][:addUser][:user]).to have_key(:email)
+    end
+  end
+
+end

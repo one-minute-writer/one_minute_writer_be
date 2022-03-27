886c68ca5ea1453a47901d785c500d3033b48051
Kevin Gloss <kgloss89@gmail.com>
2022-03-27 15:08:01 -0600

    Change title faker data

--- spec/factories/story.rb
+++ spec/factories/story.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :story do
     user
-    title { Faker::Lorem.word }
+    title { Faker::Book.title }
     total_time_in_seconds { Faker::Number.number(digits: 3) }
     body_text { Faker::Lorem.paragraph(sentence_count: 4) }
     word { Faker::Lorem.word }

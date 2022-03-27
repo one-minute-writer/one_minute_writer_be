9ef159c43dda915789d55f91d7db40a7a5c7d77d
Kevin Gloss <kgloss89@gmail.com>
2022-03-27 15:29:18 -0600

    Add template for story mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
+    field :delete_story, mutation: Mutations::DeleteStory
+    field :update_story, mutation: Mutations::UpdateStory
+    field :create_story, mutation: Mutations::CreateStory
     field :create_user, mutation: Mutations::CreateUser
     field :update_user, mutation: Mutations::UpdateUser
     field :delete_user, mutation: Mutations::DeleteUser

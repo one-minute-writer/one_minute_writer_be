91df654fe7dfe4a84aadd2762703bd33c5909101
Dylan Harper <dylanharper4@icloud.com>
2022-03-28 12:09:05 -0700

    Add mutation to create story

--- app/graphql/mutations/stories/create_story.rb
+++ app/graphql/mutations/stories/create_story.rb
@@ -1,16 +1,27 @@
 module Mutations
   module Stories
     class CreateStory < Mutations::BaseMutation
-      # TODO: define return fields
-      # field :post, Types::PostType, null: false
+      argument :user_id, Integer, required: true
+      argument :title, String, required: true
+      argument :body_text, String, required: true
+      argument :word, String, required: true
+      argument :image, String, required: true
+      argument :sound, String, required: true
+      argument :total_time_in_seconds, Integer, required: true
 
-      # TODO: define arguments
-      # argument :name, String, required: true
+      field :story, Types::StoryType, null: false
 
-      # TODO: define resolve method
-      # def resolve(name:)
-      #   { post: ... }
-      # end
+      def resolve(attributes)
+        begin
+          user = User.find(attributes[:user_id])
+          story = user.stories.create!(attributes)
+
+          { story: story }
+        rescue ActiveRecord::RecordInvalid => e
+          GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
+            " #{e.record.errors.full_messages.join(', ')}")
+        end
+      end
     end
   end
 end

f34a8ea0620cf1830dc35800b364a11143038767
Dylan Harper <dylanharper4@icloud.com>
2022-03-28 10:35:18 -0700

    Add stories field to user type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,5 +3,6 @@ module Types
     field :id, ID, null: false
     field :username, String, null: false
     field :email, String, null: false
+    field :stories, [Types::StoryType], null: false
   end
 end

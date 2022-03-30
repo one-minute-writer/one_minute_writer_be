3464a250dbe417422388a88acf8d0fe6fe4c3412
Kevin Gloss <kgloss89@gmail.com>
2022-03-30 15:11:15 -0600

    Update test format

--- spec/graphql/mutations/users/create_user_spec.rb
+++ spec/graphql/mutations/users/create_user_spec.rb
@@ -19,26 +19,16 @@ module Mutations
         end
 
         it 'updates a user' do
+          expect(User.count).to eq(0)
+
           post '/graphql', params: { query: query }
 
           data = parse_json[:data]
 
-          expect(current_user_count).to eq(user_record_length + 1)
-          expect(symbolized).to be_a Hash
-          expect(symbolized[:data][:createUser][:user]).to have_key(:username)
-          expect(symbolized[:data][:createUser][:user]).to have_key(:email)
-        end
-
-        it 'can create a user' do
-          user_record_length = User.all.length
-          result = OneMinuteWriterBeSchema.execute(query).as_json
-          symbolized = result.deep_symbolize_keys!
-          current_user_count = User.all.length
-
-          expect(current_user_count).to eq(user_record_length + 1)
-          expect(symbolized).to be_a Hash
-          expect(symbolized[:data][:createUser][:user]).to have_key(:username)
-          expect(symbolized[:data][:createUser][:user]).to have_key(:email)
+          expect(User.count).to eq(1)
+          expect(data).to be_a Hash
+          expect(data[:createUser][:user]).to have_key(:username)
+          expect(data[:createUser][:user]).to have_key(:email)
         end
       end
     end

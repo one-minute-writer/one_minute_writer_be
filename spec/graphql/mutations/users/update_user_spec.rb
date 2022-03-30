655d98f6c19755362a1e0442142adaceaca8eab2
Kevin Gloss <kgloss89@gmail.com>
2022-03-30 15:38:32 -0600

    Test sad path

--- spec/graphql/mutations/users/update_user_spec.rb
+++ spec/graphql/mutations/users/update_user_spec.rb
@@ -24,6 +24,27 @@ module Mutations
             GQL
           end
 
+          def bad_query
+          <<~GQL
+          mutation {
+            updateUser(
+              input: {
+                id: "1001",
+                username: 10,
+                email: "blees@gmail.com",
+                location: "here"
+                })
+                { user {
+                  username
+                  id
+                  email
+                  location
+                }
+              }
+            }
+            GQL
+          end
+
         it 'updates a user' do
           @bob = create(:user, id: 1001, username: "Bob", email: "Bobathan@gmail.com")
 
@@ -34,6 +55,28 @@ module Mutations
           expect(data[:updateUser][:user][:id]).to eq(@bob.id.to_s)
           expect(data[:updateUser][:user][:username]).to eq('Bob Lee Swagger')
           expect(data[:updateUser][:user][:email]).to eq('blees@gmail.com')
+        end
+
+        it 'user does not exist (sad path)' do
+          @bob = create(:user, id: 1000, username: "Bob", email: "Bobathan@gmail.com")
+
+          post '/graphql', params: { query: query }
+
+          message = parse_json[:errors].first[:message]
+
+          expect(message).to eq("User does not exist.")
+        end
+
+        it 'does not update with other attributes (sad path)' do
+          @bob = create(:user, id: 1001, username: "Bob", email: "Bobathan@gmail.com")
+
+          post '/graphql', params: { query: bad_query }
+
+          message = parse_json[:errors]
+
+          expect(message.first[:message]).to eq("Argument 'username' on InputObject 'UpdateUserInput' has an invalid value. Expected type 'String'.")
+          expect(message.second[:message]).to eq("InputObject 'UpdateUserInput' doesn't accept argument 'location'")
+          expect(message.third[:message]).to eq("Field 'location' doesn't exist on type 'User'")
         end
       end
     end

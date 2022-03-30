4c4f45a344486ff95ed6af0ebc6b99767f4e4655
Kevin Gloss <kgloss89@gmail.com>
2022-03-30 15:02:10 -0600

    Test fetch user

---
+++ spec/graphql/queries/user/fetch_user_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe Types::QueryType, type: :request do
+  def query
+    <<~GQL
+      {
+        fetchUser(id: #{User.first.id}) {
+        id
+        username
+        email
+      }
+    }
+    GQL
+  end
+
+  def bad_query
+    <<~GQL
+      {
+        fetchUser(id: 0) {
+        id
+        username
+        email
+      }
+    }
+    GQL
+  end
+
+  it 'gets a user by id' do
+    create_list(:user, 4)
+    
+    post '/graphql', params: { query: query }
+
+    data = parse_json[:data]
+
+    expect(data[:fetchUser][:id]).to eq(User.first.id.to_s)
+    expect(data[:fetchUser][:username]).to eq(User.first.username)
+    expect(data[:fetchUser][:email]).to eq(User.first.email)
+  end
+
+  it 'throws an error if user does not exist' do
+    create_list(:user, 4)
+    
+    post '/graphql', params: { query: bad_query }
+
+    data = parse_json
+
+    expect(data[:errors].first[:message]).to eq('User does not exist.')
+  end
+end

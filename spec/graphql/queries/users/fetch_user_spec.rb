76ad0b25983b8a55c69e4cc51ab6420b6957a313
Kevin Gloss <kgloss89@gmail.com>
2022-03-30 15:11:08 -0600

    Update formatting

--- spec/graphql/queries/users/fetch_user_spec.rb
+++ spec/graphql/queries/users/fetch_user_spec.rb
@@ -1,49 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe FetchUsers, type: :request do
-  def query
-    <<~GQL
-      {
-        fetchUser(id: #{User.first.id}) {
-        id
-        username
-        email
-      }
-    }
-    GQL
-  end
+module Queries
+  module Users 
+    RSpec.describe FetchUser, type: :request do
+      def query
+        <<~GQL
+          {
+            fetchUser(id: #{User.first.id}) {
+            id
+            username
+            email
+          }
+        }
+        GQL
+      end
 
-  def bad_query
-    <<~GQL
-      {
-        fetchUser(id: 0) {
-        id
-        username
-        email
-      }
-    }
-    GQL
-  end
+      def bad_query
+        <<~GQL
+          {
+            fetchUser(id: 0) {
+            id
+            username
+            email
+          }
+        }
+        GQL
+      end
 
-  it 'gets a user by id' do
-    create_list(:user, 4)
-    
-    post '/graphql', params: { query: query }
+      it 'gets a user by id' do
+        create_list(:user, 4)
+        
+        post '/graphql', params: { query: query }
 
-    data = parse_json[:data]
+        data = parse_json[:data]
 
-    expect(data[:fetchUser][:id]).to eq(User.first.id.to_s)
-    expect(data[:fetchUser][:username]).to eq(User.first.username)
-    expect(data[:fetchUser][:email]).to eq(User.first.email)
-  end
+        expect(data[:fetchUser][:id]).to eq(User.first.id.to_s)
+        expect(data[:fetchUser][:username]).to eq(User.first.username)
+        expect(data[:fetchUser][:email]).to eq(User.first.email)
+      end
 
-  it 'throws an error if user does not exist' do
-    create_list(:user, 4)
-    
-    post '/graphql', params: { query: bad_query }
+      it 'throws an error if user does not exist' do
+        create_list(:user, 4)
+        
+        post '/graphql', params: { query: bad_query }
 
-    data = parse_json
+        data = parse_json
 
-    expect(data[:errors].first[:message]).to eq('User does not exist.')
+        expect(data[:errors].first[:message]).to eq('User does not exist.')
+      end
+    end
   end
 end
--- spec/graphql/queries/users/fetch_users_spec.rb
+++ spec/graphql/queries/users/fetch_users_spec.rb
@@ -1,36 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe Types::QueryType, type: :request do
-  def query
-    <<~GQL
-    {
-      fetchUsers {
-        id
-        username
-        email
-      }
-    }
-    GQL
-  end
+module Queries
+  module Users 
+    RSpec.describe FetchUser, type: :request do
+      def query
+        <<~GQL
+        {
+          fetchUsers {
+            id
+            username
+            email
+          }
+        }
+        GQL
+      end
 
-  it 'gets all users' do
-    create_list(:user, 4)
-    
-    post '/graphql', params: { query: query }
+      it 'gets all users' do
+        create_list(:user, 4)
+        
+        post '/graphql', params: { query: query }
 
-    users = parse_json[:data][:fetchUsers]
+        users = parse_json[:data][:fetchUsers]
 
-    expect(users.count).to eq(User.count)
-    expect(users.reverse.first[:id]).to eq(User.first.id.to_s)
-    expect(users.reverse.first[:username]).to eq(User.first.username)
-    expect(users.reverse.first[:email]).to eq(User.first.email)
-  end
+        expect(users.count).to eq(User.count)
+        expect(users.reverse.first[:id]).to eq(User.first.id.to_s)
+        expect(users.reverse.first[:username]).to eq(User.first.username)
+        expect(users.reverse.first[:email]).to eq(User.first.email)
+      end
 
-  it 'no users' do
-    post '/graphql', params: { query: query }
+      it 'no users' do
+        post '/graphql', params: { query: query }
 
-    users = parse_json[:data][:fetchUsers]
+        users = parse_json[:data][:fetchUsers]
 
-    expect(users.empty?).to be(true)
+        expect(users.empty?).to be(true)
+      end
+    end
   end
 end

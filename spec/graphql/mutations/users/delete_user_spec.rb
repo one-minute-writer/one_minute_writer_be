fd7d49d4af07a2dc5f183b74aafcdfc7638c7882
Kevin Gloss <kgloss89@gmail.com>
2022-03-30 15:24:25 -0600

    Test sad path, user not found

--- spec/graphql/mutations/users/delete_user_spec.rb
+++ spec/graphql/mutations/users/delete_user_spec.rb
@@ -18,6 +18,21 @@ module Mutations
         GQL
       end
 
+      def bad_query
+        <<~GQL
+          mutation {
+          deleteUser(
+            input: {
+              id: "0",
+            }
+          ) { user {
+              username
+            }
+          }
+        }
+        GQL
+      end
+
       describe '.resolve' do
         it 'deletes a user' do
           @bob = create(:user, username: "Bob", email: "Bobathan@gmail.com")
@@ -27,6 +42,16 @@ module Mutations
           data = parse_json[:data]
 
           expect(data[:deleteUser][:user][:username]).to eq('Bob')
+        end
+
+        it 'can not find user to delete' do
+          @bob = create(:user, username: "Bob", email: "Bobathan@gmail.com")
+
+          post '/graphql', params: { query: bad_query }
+
+          message = parse_json[:errors].first[:message]
+
+          expect(message).to eq("Could not find user.")
         end
       end
     end

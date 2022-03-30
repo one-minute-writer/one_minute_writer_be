9b86fb3424f717fa2577ff7ef95cc19174b38787
Kevin Gloss <kgloss89@gmail.com>
2022-03-30 15:38:44 -0600

    Rescue from record not found

--- app/graphql/mutations/users/update_user.rb
+++ app/graphql/mutations/users/update_user.rb
@@ -13,6 +13,8 @@ module Mutations
           user.update!(attributes)
 
           {user: user}
+        rescue ActiveRecord::RecordNotFound => _e
+          GraphQL::ExecutionError.new('User does not exist.')
         rescue ActiveRecord::RecordInvalid => e
           GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
             " #{e.record.errors.full_messages.join(', ')}")

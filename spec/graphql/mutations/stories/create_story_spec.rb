a2a134151f6cbffacdbe97c829f8064bf6e2620d
Dylan Harper <dylanharper4@icloud.com>
2022-03-30 22:02:45 -0700

    Add sad path testing for create story spec

--- spec/graphql/mutations/stories/create_story_spec.rb
+++ spec/graphql/mutations/stories/create_story_spec.rb
@@ -28,6 +28,28 @@ module Mutations
           GQL
         end
 
+        def bad_query
+          <<~GQL
+            mutation {
+              createStory(input:
+              { title: "Thoughts",
+                bodyText: "hello world",
+                word: "test",
+                sound: "echo",
+                totalTimeInSeconds: 120 } ) {
+            	story {
+              	title
+              	bodyText
+                word
+                image
+                sound
+                totalTimeInSeconds
+                }
+              }
+            }
+          GQL
+        end
+
         it 'can create a story' do
           create_list(:user, 5)
 
@@ -41,6 +63,17 @@ module Mutations
           expect(story).to have_key(:image)
           expect(story).to have_key(:sound)
           expect(story).to have_key(:totalTimeInSeconds)
+        end
+
+        it 'only accepts correct attributes' do
+          create_list(:user, 5)
+
+          post '/graphql', params: { query: bad_query }
+
+          response = parse_json
+
+          expect(response[:errors].first[:message]).to eq("Argument 'userId' on InputObject 'CreateStoryInput' is required. Expected type Int!")
+          expect(response[:errors].last[:message]).to eq("Argument 'image' on InputObject 'CreateStoryInput' is required. Expected type String!")
         end
       end
     end

546b1dfa75eaac5916c4b2f986d6a88fba7b7c8d
Dylan Harper <dylanharper4@icloud.com>
2022-04-04 16:08:31 -0700

    Get user metrics to update via python server

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ApplicationRecord
   validates_presence_of :username, :email
   validates_uniqueness_of :email
 
+
+  def update_metrics
+    story_ids = []
+
+    self.stories.each do |story|
+      story_ids << story.id
+    end
+
+    metrics = DashboardMetricsFacade.get_writing_metrics(story_ids.join(','))
+
+    self.dashboard_metrics[:total_words_all_time] = metrics.total_words_all_time
+    self.dashboard_metrics[:total_time] = metrics.total_time
+  end
 end

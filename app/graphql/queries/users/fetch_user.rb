83c6f29ad3216e3160b5be3f8a0014f4057c8f15
Brad Breiten <jonathan@breiten.us>
2022-04-04 17:47:13 -0500

    Add dashboard metrics get to fetch user controller

--- app/graphql/queries/users/fetch_user.rb
+++ app/graphql/queries/users/fetch_user.rb
@@ -5,16 +5,34 @@ module Queries
       argument :id, ID, required: true
 
       def resolve(id:)
-        User.find(id)
+        begin
+          user = User.find(id)
+
+          story_ids = []
+
+          user.stories.each do |story|
+            story_ids << story.id
+          end
+
+          metrics = DashboardMetricsFacade.get_writing_metrics(story_ids.join(','))
+
+          user.dashboard_metrics[:total_words_all_time] = metrics.total_words_all_time
+          user.dashboard_metrics[:total_time] = metrics.total_time
+
+          {user: user}
+
+
+
         #user has to have updated metrics
         #AR to get array of story ids
         #send array to python/django
         #add return value as an attribute to user (ie 'dashboard_metrics')
-      rescue ActiveRecord::RecordNotFound => _e
-        GraphQL::ExecutionError.new('User does not exist.')
-      rescue ActiveRecord::RecordInvalid => e
-        GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
-          " #{e.record.errors.full_messages.join(', ')}")
+        rescue ActiveRecord::RecordNotFound => _e
+          GraphQL::ExecutionError.new('User does not exist.')
+        rescue ActiveRecord::RecordInvalid => e
+          GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
+            " #{e.record.errors.full_messages.join(', ')}")
+        end
       end
     end
   end
---
+++ spec/graphql/mutations/users/user_metrics_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+module Mutations
+  module Users
+    RSpec.describe CreateUser, type: :request do
+      describe 'create user' do
+        def query
+          <<~GQL
+            mutation {
+              createUser(
+                input: {
+                  username: "Lassie",
+                  email: "sillyboy@inwell.com"
+                }
+              )
+              {
+                user {
+                    username
+                    email
+                    dashboardMetrics
+                }
+              }
+            }
+          GQL
+        end
+
+        def bad_query
+          <<~GQL
+            mutation
+            {
+              createUser(
+                input: {
+                  username: "",
+                  email: "sillyboy@inwell.com"
+                }
+              )
+              {
+                user {
+                  username
+                  email
+                }
+              }
+            }
+          GQL
+        end
+
+        it 'returns user metrics from python service' do
+          post '/graphql', params: { query: query }
+
+          data = parse_json[:data]
+
+          create_list(:story, 5, user_id: User.first.id)
+
+          require "pry"; binding.pry
+        end
+      end
+    end
+  end
+end

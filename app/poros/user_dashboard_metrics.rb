eb34d5099989a87bf0bbd38d7f38525bdf3526b6
Brad Breiten <jonathan@breiten.us>
2022-04-04 21:00:44 -0500

    Remove #hours_minutes_seconds method from user_dashbaord_metrics PORO. Un-needed.

--- app/poros/user_dashboard_metrics.rb
+++ app/poros/user_dashboard_metrics.rb
@@ -9,15 +9,5 @@ class UserDashboardMetrics
     @total_time = data[:total_time_all_time]
     @average_words_per_minute = data[:average_words_per_minute]
   end
-
-  def hours_minutes_and_seconds
-    hours = @total_time / (60*60) # number of seconds in a minute x number of minutes in an hour, 3600 seconds.
-    minutes = (@total_time / 60) % 60 # the modulo operator (%) gives the remainder when leftside is divided by rightside. Ex: 121 % 60 = 1
-    seconds = @total_time % 60
-    [hours, minutes, seconds].map do |t|
-      # Right justify and pad with 0 until length is 2.
-      # So if the duration of any of the time components is 0, then it will display as 00
-      t.round.to_s.rjust(2,'0')
-    end.join(':')
-  end
+  
 end
--- spec/poros/user_dashboard_metrics_spec.rb
+++ spec/poros/user_dashboard_metrics_spec.rb
@@ -11,16 +11,4 @@ RSpec.describe UserDashboardMetrics do
     expect(metrics.average_words_per_minute).to eq 74
   end
 
-  describe 'instance methods' do
-    describe 'hours_minutes_and_seconds' do
-      it 'turns the total_time variable into a string of hours, minutes, and seconds' do
-        expect(metrics.hours_minutes_and_seconds).to eq('00:01:00')
-      end
-
-      it 'works for larger numbers' do
-        metrics_1 = UserDashboardMetrics.new({:total_words_all_time=>50, :total_time_all_time=>789000})
-        expect(metrics_1.hours_minutes_and_seconds).to eq("219:10:00")
-      end
-    end
-  end
 end
